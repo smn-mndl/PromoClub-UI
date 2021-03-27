1b2eb66c9cb4c933e5ebef9461231bc614084a99
smn-mndl <45252675+smn-mndl@users.noreply.github.com>
2021-03-28 00:01:57 +0530

    download by file names

--- src/js/scenes/user-cart/UserCart.js
+++ src/js/scenes/user-cart/UserCart.js
@@ -226,7 +226,10 @@ const UserCart = () => {
         const url = window.URL.createObjectURL(new Blob([response.data]));
         const link = document.createElement("a");
         link.href = url;
-        link.setAttribute("download", "test.jpg");
+        link.setAttribute(
+          "download",
+          `${eachCartElem["photoDtls"]["attributes"]["title"]}.jpg`
+        );
         document.body.appendChild(link);
         link.click();
       });
